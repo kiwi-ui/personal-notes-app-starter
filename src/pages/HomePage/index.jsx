aa059ce4259d19f82e475842492c228a56fe533f
yusni anggara <yusnianggara@gmail.com>
2025-05-09 17:15:43 +0700

    feat: search notes done

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,80 +1,103 @@
-import React, { useEffect, useState } from 'react'
-import { addNote, archiveNote, deleteNote, getActiveNotes, getAllNotes, getArchivedNotes, getNote, unarchiveNote } from '../../utils/local-data'
-import { useNavigate } from 'react-router-dom';
-import AddNotePage from '../AddNotePage';
+import React, { useEffect, useState } from 'react';
+import { archiveNote, deleteNote, getActiveNotes } from '../../utils/local-data'
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 const HomePage = () => {
   const [Contacts, setContacts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get('keyword' || '');
   const navigate = useNavigate();
 
   useEffect(() => {
-    setContacts(getActiveNotes());
-  }, [])
+	const activeNotes = getActiveNotes();
+	  if (keyword) {
+		const filteredNotes = activeNotes.filter((note) =>
+		  note.title.toLowerCase().includes(keyword.toLowerCase()) ||
+		  note.body.toLowerCase().includes(keyword.toLowerCase())
+		);
+		setContacts(filteredNotes);
+	  } else {
+		setContacts(activeNotes);
+	  }
+  }, [keyword])
+
+
+  const handleSearch = (query) => {
+//   const activeNotes = getActiveNotes();
+
+//   const notes = activeNotes.filter((note) => note.body.toLocaleLowerCase().includes(event)) || note.title.toLowerCase().includes(event.toLowerCase());
+	setSearchParams({keyword: query})
+	// setContacts(notes);
+  }
 
   const handleGetDetailed = (id) => {
-		navigate(`/detail/${ id }`);
+	navigate(`/detail/${ id }`);
   }
 
   const handleDeleteNote = (id) => {
-		deleteNote(id);
-		setContacts(getActiveNotes);
+	const confirmDelete = window.confirm('Yakin ingin menghapus catatan ini?');
+	if(confirmDelete){
+	  deleteNote(id);
+	  setContacts(getActiveNotes());
+	}
   }
 
-	
   const handleArchive = (id) => {
-		archiveNote(id);
-		setContacts(getActiveNotes());
-   }
+	archiveNote(id);
+	setContacts(getActiveNotes());
+  }
 
   return (
     <main className="main">
-				<button type="button" onClick={ () => navigate('/notes/new')}>Tambah Catatan</button>
-				<button type="button" onClick={ () => navigate('/archive') }>Archived pages</button>
-		
-				<div className="app-container body">
-					{ 
-						Contacts.length ? 
-							<div className="notes-list">
+		<button type="button" onClick={ () => navigate('/notes/new')}>Tambah Catatan</button>
+		<button type="button" onClick={ () => navigate('/archive') }>Archived pages</button>
+		<SearchBar onSearch={ handleSearch }/>
+
+		<div className="app-container body">
+			{ 
+				Contacts.length ? 
+					<div className="notes-list">
+						{
+							Contacts.map((contact) =>
 								{
-									Contacts.map((contact) =>
-										{
-											return (
-												<div key={ contact.id } className="note-item">
-														<div className="note-item__title">
-															{ contact.title }
-														</div>
-
-														<div className="note-item__createdAt">
-															{
-																new Date(contact.createdAt).toLocaleDateString("id-ID", {
-																	day: "numeric",
-																	month: "long",
-																	year: "numeric"
-																}) 
-															}
-														</div>
-
-														<div className="note-item__body">
-															{ contact.body }
-														</div>
-
-														<button type="button" onClick={ () => handleDeleteNote(contact.id) }>Delete</button>
-														<button type="button" onClick={ () => handleArchive(contact.id) }>Simpan</button>
-														<button type="button" onClick={ () => handleGetDetailed(contact.id) }>See detail</button>	
+									return (
+										<div key={ contact.id } className="note-item">
+												<div className="note-item__title">
+													{ contact.title }
 												</div>
-											) 
-										}
-									)
+
+												<div className="note-item__createdAt">
+													{
+														new Date(contact.createdAt).toLocaleDateString("id-ID", {
+															day: "numeric",
+															month: "long",
+															year: "numeric"
+														}) 
+													}
+												</div>
+
+												<div className="note-item__body">
+													{ contact.body }
+												</div>
+
+												<button type="button" onClick={ () => handleDeleteNote(contact.id) }>Delete</button>
+												<button type="button" onClick={ () => handleArchive(contact.id) }>Simpan</button>
+												<button type="button" onClick={ () => handleGetDetailed(contact.id) }>See detail</button>	
+										</div>
+									) 
 								}
-							</div> 
+							)
+						}
+					</div> 
 
-							:
+					:
 
-							<div className="note-list-empty">
-								<p className="p">"Tidak ada catatan"</p> 
-							</div> 
-					}
-				</div>
+					<div className="note-list-empty">
+						<p className="p">"Tidak ada catatan"</p> 
+					</div> 
+			}
+		</div>
     </main>
   )
 }
